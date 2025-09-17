1cc92bed9eb22a4e1d99d3e4babe70ac746d67c2
Ritik Rana <ritikrana4@gmail.com>
2025-09-17 23:32:51 +0530

    use memmo

---
+++ LLD/src/hooks/useMemo.js
@@ -0,0 +1,33 @@
+import {  useEffect, useRef } from "react"
+
+const dependChange = (prevDep,newDep) => {
+    if(prevDep === null) return false
+    if(prevDep?.length !== newDep.length) return false
+    for(let i = 0;i<newDep.length;i++){
+        if(prevDep[i] !== newDep[i]){
+            return false
+        }
+    }
+    return true
+}
+export const useMyMemo = (cb,depArr) => {
+    
+    const memoRef = useRef()
+
+    //check dep change
+    if(!memoRef.current || !dependChange(memoRef.current.dep,depArr)){
+        memoRef.current = {
+            val:cb(),
+            dep:depArr
+        }
+    }
+
+    useEffect(()=>{
+        return ()=>{
+            memoRef.current = null
+        }
+    },[])
+
+    return memoRef.current.val
+
+}
